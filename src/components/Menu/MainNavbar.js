327395bf991718d3c48d9fb78756467dba0aede5
unknown <mariaekmaan91@gmail.com>
2025-04-16 23:59:10 +0200

    ta bort filer

--- src/components/Menu/MainNavbar.js
+++ src/components/Menu/MainNavbar.js
@@ -5,8 +5,9 @@ import CasePageTwo from "./CasePageTwo";
 import CasePageOne from "./CasePageOne";
 import CasePageThree from "./CasePageThree"
 import CasePageFour from "./CasePageFour";
-import Navbar from "./Navbar";import ImageGrid from "./ImageGrid";
-
+import Navbar from "./Navbar";
+import ImageGrid from "./ImageGrid";
+import Homepage from '../../Page/Homepage';
  import AboutPages from "./AboutMe"
 
 export default function MainNavbar() {
@@ -15,7 +16,7 @@ export default function MainNavbar() {
       <Navbar />
       
         <Routes>
-          <Route path="/"  element={<HomePage/>} />
+          <Route path="/"  element={<Homepage/>} />
           <Route path="/about"  element={<AboutPages />} />
           <Route path="/case"  element={<ImageGrid/>} />
           <Route path="/case1" element={<CasePageOne />} />
